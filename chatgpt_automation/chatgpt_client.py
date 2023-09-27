65ebe08604a053a70c6fab6875dd8d1433c075b0
Hai Dang <hopthucuahaidang@gmail.com>
2023-09-27 23:43:12 +0700

    fix detect chrome version

--- chatgpt_automation/chatgpt_client.py
+++ chatgpt_automation/chatgpt_client.py
@@ -357,7 +357,8 @@ class ChatGPT_Client:
             raise RuntimeError('Unable to find the text prompt area. Please raise an issue with verbose=True')
             return
         text_area = text_area[0]
-        text_area[0].clear()
+        text_area.send_keys('')
+        self.browser.execute_script(f'document.getElementById("{self.textarea_iq}").value="";')
 
         for each_line in question.split('\n'):
             text_area.send_keys(each_line)
